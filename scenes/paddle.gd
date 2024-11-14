ab517e77cb6b36b04487d14e7a8cc19854708a5f
Artur Gawron <artur@gawron.me>
2024-11-14 01:45:09 +0100

    refactor(script): moving paddle

--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -9,8 +9,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("left"):
-		position.x = position.x - speed * delta
-		
-	elif Input.is_action_pressed("right"):
-		position.x = position.x + speed * delta
+	#if Input.is_action_pressed("left"):
+		#position.x = position.x - speed * delta
+		#
+	#elif Input.is_action_pressed("right"):
+		#position.x = position.x + speed * delta
+	var input_amount: float = Input.get_axis("left", "right")
+	position.x += delta * speed * input_amount
