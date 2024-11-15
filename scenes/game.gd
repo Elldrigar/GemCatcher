59c14334e5a07d25b25f90cf54636e98e4d66170
Artur Gawron <artur@gawron.me>
2024-11-15 22:49:05 +0100

    feat(script): add scoring

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,22 +1,30 @@
 extends Node2D
 
 @export var gem_scene: PackedScene
+@onready var label: Label = $Label
 
-# Called when the node enters the scene tree for the first time.
+var _score: int = 0
+
 func _ready() -> void:
-	_spawn_gem()
+	spawn_gem()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-func _spawn_gem() -> void:
-	var new_gem = gem_scene.instantiate()
+func spawn_gem() -> void:
+	var new_gem: Gem = gem_scene.instantiate()
 	var xpos: float = randf_range(70, 1050)
-	new_gem.on_gem_off_screen.connect()
+	new_gem.on_gem_off_screen.connect(game_over)
 	new_gem.position = Vector2(xpos, -50)
 	add_child(new_gem)
 
+func game_over() -> void:
+	print("GAME OVER")
+
 func _on_timer_timeout() -> void:
-	_spawn_gem()
+	spawn_gem()
+
+func _on_paddle_area_entered(area: Area2D) -> void:
+	_score += 1
+	label.text = "%04d" % _score
+	area.queue_free()
--- scenes/paddle.gd
+++ scenes/paddle.gd
@@ -2,12 +2,9 @@ extends Area2D
 
 @export var speed: float = 600.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#if Input.is_action_pressed("left"):
 		#position.x = position.x - speed * delta
@@ -16,7 +13,3 @@ func _process(delta: float) -> void:
 		#position.x = position.x + speed * delta
 	var input_amount: float = Input.get_axis("left", "right")
 	position.x += delta * speed * input_amount
-
-
-func _on_gem_area_entered(area: Area2D) -> void:
-	print(area) # Replace with function body.
